fec2571d50040c33b952e3c394d921b3c674a237
Adam Collard <adam.collard@gmail.com>
2012-08-18 15:23:00 +0300

    Line length

--- rb_stats.py
+++ rb_stats.py
@@ -21,24 +21,33 @@ def parse_req_params(pargs):
 def run_main():
     parser = argparse.ArgumentParser(description='Review Board Stats')
     parser.add_argument('base_api_url', help='The ReviewBoard base API URL')
-    parser.add_argument('--from-user', help='Only Review Requests by this user')
+    parser.add_argument('--from-user',
+                        help='Only Review Requests by this user')
     parser.add_argument('--max-results', type=int,
-        help='Maximum number of Review Requests to retrieve (default: %(default)s)',
-        default=200)
-    parser.add_argument('--status', choices=['discarded', 'pending', 'submitted'],
-        help='Only retrieve Review Requests with this status (default: %(default)s)',
-        default='submitted')
+                        help=('Maximum number of Review Requests to '
+                              'retrieve (default: %(default)s)'),
+                        default=200)
+    parser.add_argument('--status',
+                        choices=['discarded', 'pending', 'submitted'],
+                        help=('Only retrieve Review Requests with '
+                              'this status (default: %(default)s)'),
+                        default='submitted')
     parser.add_argument('--colormap',
-        help='The colormap that should be used for the plotting (default: %(default)s)',
-        default='jet')
-    parser.add_argument('--interpolation', help='Interpolation method to use (default: %(default)s',
-        default='gaussian', choices=['none', 'nearest', 'bilinear', 'bicubic',
-        'hanning', 'hamming', 'gaussian'])
+                        help=('The colormap that should be used for '
+                              'the plotting (default: %(default)s)'),
+                        default='jet')
+    parser.add_argument('--interpolation',
+                        help=('Interpolation method to use '
+                              '(default: %(default)s'),
+                        default='gaussian',
+                        choices=['none', 'nearest', 'bilinear', 'bicubic',
+                                 'hanning', 'hamming', 'gaussian'])
 
     args = parser.parse_args()
     req_params = parse_req_params(vars(args))
 
-    ret = requests.get(args.base_api_url + '/review-requests', params=req_params)
+    ret = requests.get(args.base_api_url + '/review-requests',
+                       params=req_params)
     js = json.loads(ret.content)
 
     graph_data = defaultdict(int)
@@ -61,7 +70,8 @@ def run_main():
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    im = ax.imshow(arr, cmap=plt.get_cmap(args.colormap), interpolation=args.interpolation)
+    im = ax.imshow(arr, cmap=plt.get_cmap(args.colormap),
+                   interpolation=args.interpolation)
     ax.set_yticklabels(xaxis)
 
     tick_locs = range(len(yaxis))
